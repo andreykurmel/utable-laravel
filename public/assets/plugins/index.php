d3ac54b65c697a8714421f8a7c20ece5975113e6
Andrey K <andrey.kurmel@faceit-team.com>
2018-02-05 09:11:06 +0200

    add home page to website root.

---
+++ public/assets/plugins/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en" style="position:relative;height: 100%;width: 100%;">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+    
+    <link href="home/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="home/assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <title>Utable</title>
+</head>
+<body style="position:relative;height: 100%;width: 100%;">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div id="navbar" class="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right" style="float: right;">
+                    <li style="display: inline-block">
+                        <a href="home/login">
+                            <i class="fa fa-sign-in"></i>
+                            Login
+                        </a>
+                    </li>
+                    <li style="display: inline-block">
+                        <a href="home/register">
+                            <i class="fa fa-user-plus"></i>
+                            Register
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    <div style="position: absolute;top: 40%;text-align: center;width: 100%;">
+        <h1>UTable - your table data in one place</h1>
+    </div>
+</body>
+</html>
