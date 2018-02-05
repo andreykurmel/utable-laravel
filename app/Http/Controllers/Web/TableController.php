e8130a831083296465f1466e20d17b6f78a3fa55
Andrey K <andrey.kurmel@faceit-team.com>
2018-02-05 16:06:11 +0200

    Fix point 7.

--- app/Http/Controllers/Web/TableController.php
+++ app/Http/Controllers/Web/TableController.php
@@ -62,7 +62,7 @@ class TableController extends Controller
 
         $params = $request->except(['id', 'tableName']);
         foreach ($params as $key => $par) {
-            if (strpos('/', $key)) {
+            if (strpos('/', $key) === false) {
                 unset($params[$key]);
             }
         }
@@ -87,7 +87,7 @@ class TableController extends Controller
 
         $params = $request->except(['id', 'tableName']);
         foreach ($params as $key => $par) {
-            if (strpos('/', $key)) {
+            if (strpos('/', $key) === false) {
                 unset($params[$key]);
             }
         }
